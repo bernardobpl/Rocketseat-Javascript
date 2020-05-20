31fc90485df3c363d36f93715f0624d1cb116f28
Jajack <bernardobpl97@gmail.com>
2020-05-20 18:38:05 -0300

    Ex 3 e add extra no 1

--- Módulo 1/Exercício 1/script.js
+++ Módulo 1/Exercício 1/script.js
@@ -15,7 +15,12 @@ Retorne o seguinte conteúdo:
 O usuário mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com nº 1293. 
 */
 
-var p = document.querySelector('p');
+const p = document.querySelector('p');
+const rua = document.getElementById('Rua');
+const numero = document.getElementById('Numero');
+const bairro = document.getElementById('Bairro');
+const cidade = document.getElementById('Cidade');
+const uf = document.getElementById('Uf');
 
 var endereco = {
     rua: "Rua dos pinheiros",
@@ -25,8 +30,21 @@ var endereco = {
     uf: "SP"
 };
 
-function adress(endereco){
+function novoend(){
+    console.log("entrou");
+    endereco = {
+        rua: rua.value,
+        numero: numero.value,
+        bairro: bairro.value,
+        cidade: cidade.value,
+        uf: uf.value,
+    }
+
+    p.innerHTML = adress();
+}
+
+function adress(){
     return `O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com nº ${endereco.numero}.` 
 }
 
-p.innerHTML = adress(endereco);
+p.innerHTML = adress();
---
+++ Módulo 1/Exercício 3/script.js
@@ -0,0 +1,53 @@
+/*
+3º exercício
+Escreva uma função que verifique se o vetor de habilidades passado possui a habilidade "Javascript"
+e retorna um booleano true/false caso exista ou não.
+
+function temHabilidade(skills) {
+ // código aqui
+}
+var skills = ["Javascript", "ReactJS", "React Native"];
+temHabilidade(skills); // true ou false
+
+Dica: para verificar se um vetor contém um valor, utilize o método indexOf.
+*/
+
+const ul = document.querySelector('ul');
+
+var skills = ["eoq","xups","myne","eggs"];
+
+function mostrar(){
+    ul.innerHTML='';
+    console.log(skills);
+    for (let i=0; i<skills.length; i++){
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(skills[i]));
+        ul.appendChild(li);
+    }
+}
+
+function adicionar(habilidade){
+    console.log("add");
+    skills.push(habilidade);
+    mostrar();
+}
+
+function remover(habilidade){
+    console.log("rem");
+    let index = skills.indexOf(habilidade);
+    skills.splice(index,1);
+    mostrar();
+}
+
+function temHabilidade(habilidade){
+    console.log("procura");
+    if(skills.indexOf(habilidade)>=0){
+        alert('Habilidade existe');
+        console.log(true);
+        return true;
+    }else {
+        alert('Habilidade não existe');
+        console.log(false);
+        return false;
+    }
+}
